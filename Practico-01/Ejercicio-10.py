661a069f117bcf4271c6c1fec027dac251edebdc
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2018-03-28 21:46:28 -0300

    Ejercicio-10

---
+++ Practico-01/Ejercicio-10.py
@@ -0,0 +1,14 @@
+def mas_larga(n):
+ palabra_mayor = 0
+ palabra_mostrar = ""
+ for palabra in n:
+     if palabra_mayor <= len(palabra):
+         palabra_mostrar = palabra
+         palabra_mayor = len(palabra)
+ return palabra_mostrar
+
+
+# n = input("Ingrese palabras separados por espacios:\n ")
+# n = n.split()
+# mas_larga(n)
+assert str(mas_larga("a hola vpn") == "hola")
