55af4071b3391ad4521419e6df119f77ef40ec11
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2018-04-06 17:04:54 -0300

    Ejercicio-07

--- Practico-01/Ejercicio-07.py
+++ Practico-01/Ejercicio-07.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-def inversa(s):
-    a = ""
-    for x in range(1, (len(s) + 1)):
-        a = a + s[-x]
-    return(a)
-
-def chequearInversa(i):
-    inv = inversa(i)
-    if inv == i:
-        print("Es un palindromo")
-    else:
-        print("No es un palindromo")
-
-
-s = input("Ingrese una frase: \n")
-chequearInversa(s.lower())
-
-assert chequearInversa("neuquen")
-=======
 def es_palindromo(original):
     cadena = original[::-1]
     if cadena == original:
@@ -27,5 +7,4 @@ def es_palindromo(original):
 
 
 # print(str(es_palindromo('radar')))
-assert es_palindromo('radar') == True
->>>>>>> 751396437412661de25b6bf82c4afdc1a98ae2f2
+assert es_palindromo('neuquen') == True
