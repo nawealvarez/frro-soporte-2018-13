88e4ed6928deeec4ee576144b983fd652faf1a2d
Marchetti Lautaro <marche_871@hotmail.com>
2018-04-06 16:54:14 -0300

    Ejercicio 3 practica 2

---
+++ Practico-02/Ejercicio-03.py
@@ -0,0 +1,46 @@
+import random
+
+
+class Persona:
+    nombre = "Lautaro"
+    edad = 16
+    sexo = 'H'
+    peso = 345
+    altura = 1.90
+    dni = "No definido"
+
+    def es_mayor_edad(self):
+        if self.edad > 18:
+            return True
+        else:
+            return False
+
+    def print_data(self):
+        print("Nombre: " + self.nombre)
+        print("Edad: " + str(self.edad))
+        print("Sexo: " + str(self.sexo))
+        print("Peso: " + str(self.peso))
+        print("Altura: " + str(self.altura))
+        print("DNI: " + str(self.dni))
+
+    def generar_dni(self):
+        # dni = []
+        # for i in range(0, 9):
+        #     n = random.randrange(10)
+        #     dni.append(n)
+        dni = random.randrange(100000000)
+        self.dni = dni
+
+    def __init__(self):
+        self.generar_dni()
+
+
+persona = Persona()
+# persona.print_data()
+if persona.es_mayor_edad():
+    print("Es mayor")
+else:
+    print("Es menor")
+
+
+
