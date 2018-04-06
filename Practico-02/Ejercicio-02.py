12a68e4134fa538264b356b5148432081f2410f0
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2018-04-06 16:46:29 -0300

    Ejercicio-02

--- Practico-02/Ejercicio-02.py
+++ Practico-02/Ejercicio-02.py
@@ -6,20 +6,18 @@ class Circulo:
 
     def area(self):
         area = 0
-        area = cmath.pi * (self.radio**2)
-        return float("%.2f" % area)
+        area = (float("%.2f" % cmath.pi)) * (self.radio**2)
+        return area
 
     def perimetro(self):
         perimetro = 0
-        perimetro = 2 * cmath.pi * self.radio
+        perimetro = 2 * (float("%.2f" % cmath.pi)) * self.radio
         return perimetro
 
 
 x = Circulo()
 # print("El area es:" + " " + str(x.area()) + " " +"y el perimetro es:" + " " + str(x.perimetro()))
 
-#assert x.area() == 50.26
-#assert x.perimetro() == 25.132741228718345
-print(x.area())
-# print(x.perimetro())
+assert x.area() == 50.24 and x.perimetro() == 25.12
+
 
