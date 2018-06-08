47d0218c8f8235296ec47a84245642432e9c2ec9
Marche <marche_871@hotmail.com>
2018-06-08 13:15:52 -0300

    Correccion ejercicio 11

--- Practico-03/Ejercicio-11.py
+++ Practico-03/Ejercicio-11.py
@@ -1,7 +1,7 @@
 import math
 
 x = input("Ingrese dividendo: ")
-y = input("Ingrese dividendo: ")
+y = input("Ingrese divisor: ")
 
 try:
     x = int(x)
@@ -10,10 +10,10 @@ try:
     print(str(d))
 except ValueError as type:
     print(str(type))
-    print("Divide({},{})").format(str(x), str(y))
+    print("Divide({},{})".format(str(x), str(y)))
 except ArithmeticError as arit:
     print(str(arit))
-    print("Divide({},{})").format(str(x), str(y))
+    print("Divide({},{})".format(str(x), str(y)))
 except ZeroDivisionError as zero:
     print(str(zero))
-    print("Divide({},{})").format(str(x), str(y))
+    print("Divide({},{})".format(str(x), str(y)))
